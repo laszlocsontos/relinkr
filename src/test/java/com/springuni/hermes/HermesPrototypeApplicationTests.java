0748f936b3c019c6d0c648933633257fb48a5c27
Laszlo Csontos <github@laszlocsontos.com>
2018-02-28 08:25:29 +0100

    Initial commit

---
+++ src/test/java/com/springuni/hermes/HermesPrototypeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springuni.hermes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HermesPrototypeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
