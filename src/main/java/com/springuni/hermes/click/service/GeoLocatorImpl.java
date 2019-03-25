0b6741b530a8c105f40e3618abd67f42ed89c26c
Laszlo Csontos <github@laszlocsontos.com>
2019-03-25 18:03:16 +0100

    Add GeoLocator (#38)

--- src/main/java/com/springuni/hermes/click/service/GeoLocatorImpl.java
+++ src/main/java/com/springuni/hermes/click/service/GeoLocatorImpl.java
@@ -2,13 +2,33 @@ package com.springuni.hermes.click.service;
 
 import com.springuni.hermes.click.model.IpAddress;
 import com.springuni.hermes.core.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestOperations;
 
 public class GeoLocatorImpl implements GeoLocator {
 
+    private static final String GEOJS_COUNTRY_ENDPOINT =
+            "https://get.geojs.io/v1/ip/country/{ip_address}";
+
+    private final RestOperations restOperations;
+
+    @Autowired
+    public GeoLocatorImpl(RestTemplateBuilder restTemplateBuilder) {
+        this(restTemplateBuilder.build());
+    }
+
+    GeoLocatorImpl(RestOperations restOperations) {
+        this.restOperations = restOperations;
+    }
+
     @Override
     public Country lookupCountry(IpAddress ipAddress) {
-        // TODO
-        return null;
+        String countryCode = restOperations.getForObject(
+                GEOJS_COUNTRY_ENDPOINT, String.class, ipAddress.getIpAddress()
+        );
+
+        return Country.valueOf(countryCode);
     }
 
 }
