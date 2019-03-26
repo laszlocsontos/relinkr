c2fd92e45b838d98148428dd46da7ff505261aa3
Laszlo Csontos <github@laszlocsontos.com>
2019-03-26 12:46:42 +0100

    Add missing annotations (#39)

--- src/main/java/com/springuni/hermes/click/service/ClickServiceImpl.java
+++ src/main/java/com/springuni/hermes/click/service/ClickServiceImpl.java
@@ -6,9 +6,13 @@ import com.springuni.hermes.core.model.Country;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClientException;
 
 @Slf4j
+@Service
+@Transactional
 @RequiredArgsConstructor
 public class ClickServiceImpl implements ClickService {
 
