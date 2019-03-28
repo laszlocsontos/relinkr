647d84db9d88fb27318e1f0d34057a61c93b8a34
Laszlo Csontos <github@laszlocsontos.com>
2019-03-28 07:58:09 +0100

    Add vue-feather-icons (#40)

--- hermes-ui/src/views/Home.vue
+++ hermes-ui/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <b-alert show>Default Alert</b-alert>
     <img alt="Vue logo" src="../assets/logo.png">
+    <airplay-icon class="custom-class"></airplay-icon>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -9,11 +10,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { AirplayIcon } from 'vue-feather-icons'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld, AirplayIcon
   }
 }
 </script>
