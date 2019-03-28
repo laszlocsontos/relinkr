3ba422e89e9dd5259b70456647ae8b95a7adc2d5
Laszlo Csontos <github@laszlocsontos.com>
2019-03-28 08:10:42 +0100

    Add Chart.js (#40)

--- hermes-ui/src/views/Home.vue
+++ hermes-ui/src/views/Home.vue
@@ -3,6 +3,8 @@
     <b-alert show>Default Alert</b-alert>
     <img alt="Vue logo" src="../assets/logo.png">
     <airplay-icon class="custom-class"></airplay-icon>
+    <b-button variant="success" @click="fillData()">Button</b-button>
+    <line-chart :chart-data="datacollection"></line-chart>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -11,11 +13,42 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import { AirplayIcon } from 'vue-feather-icons'
+import LineChart from '@/LineChart.js'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld, AirplayIcon
+    HelloWorld, AirplayIcon, LineChart
+  },
+  data() {
+    return {
+      datacollection: null
+    }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
   }
 }
 </script>
