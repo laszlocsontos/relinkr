b72a326b64bed755634e35f6471bba2c59cc2fbd
Laszlo Csontos <github@laszlocsontos.com>
2019-03-28 07:40:14 +0100

    Add BootstrapVue (#40)

--- hermes-ui/src/views/Home.vue
+++ hermes-ui/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <b-alert show>Default Alert</b-alert>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
--- hermes-ui/vue.config.js
+++ hermes-ui/vue.config.js
@@ -1,5 +1,34 @@
-// vue.config.js
+const path = require('path')
+
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options['transformAssetUrls'] = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'img-src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        };
+
+        return options;
+      });
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'bootstrap-components': path.resolve(__dirname, 'node_modules/bootstrap-vue/es/components')
+      }
+    }
+  },
   devServer: {
     open: true,
     port: 9080,
@@ -8,4 +37,3 @@ module.exports = {
     hotOnly: true
   }
 };
-
