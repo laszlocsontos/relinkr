laszlocsontos/relinkr

commit 8570db62a467a61e93db72be3a445a99fa0b3db0

Extend JwtAuthenticationFilter with being able to recognize JWT tokens from cookies (#44)

src/main/java/com/springuni/hermes/core/security/authn/WebSecurityConfig.java
@@ -41,6 +41,8 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -131,14 +133,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(
+        RequestMatcher requiresAuthenticationRequestMatcher =
+                new NegatedRequestMatcher(new OrRequestMatcher(PUBLIC_REQUEST_MATCHERS));
+
+        return new JwtAuthenticationFilter(
+                requiresAuthenticationRequestMatcher,
                 authenticationManagerBean(),
-                jwtAuthenticationFailureHandler()
+                jwtAuthenticationFailureHandler(),
+                jwtAuthenticationTokenCookieResolver
         );
-
-        jwtAuthenticationFilter.setIgnoredRequestMatchers(PUBLIC_REQUEST_MATCHERS);
-
-        return jwtAuthenticationFilter;
     }
 
     @Override

src/main/java/com/springuni/hermes/core/security/authn/jwt/JwtAuthenticationFilter.java
@@ -1,20 +1,19 @@
 package com.springuni.hermes.core.security.authn.jwt;
 
-import java.util.List;
+import com.springuni.hermes.core.web.AjaxRequestMatcher;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 /**
@@ -24,24 +23,33 @@ import org.springframework.util.StringUtils;
 public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_TOKEN_PREFIX = "Bearer";
-    public static final String BEARER_TOKEN_ATTRIBUTE = "bearer_token";
 
-    private static final RequestMatcher AUTHORIZATION_BEARER_REQUEST_HEADER_MATCHER =
-            new AuthorizationBearerRequestHeaderMatcher();
+    public static final String BEARER_TOKEN_PREFIX = "Bearer";
+
+    private static final RequestMatcher AJAX_REQUEST_MATCHER = new AjaxRequestMatcher();
+
+    private static final RequestMatcher AUTHORIZATION_HEADER_MATCHER =
+            new RequestHeaderRequestMatcher(AUTHORIZATION_HEADER);
 
     private static final AuthenticationSuccessHandler NOOP_AUTH_SUCCESS_HANDLER =
-            (request, response, authentication) -> {};
+            (request, response, authentication) -> {
+            };
+
+    private final JwtAuthenticationTokenCookieResolver authenticationTokenCookieResolver;
 
     public JwtAuthenticationFilter(
+            RequestMatcher requiresAuthenticationRequestMatcher,
             AuthenticationManager authenticationManager,
-            AuthenticationFailureHandler authenticationFailureHandler) {
+            AuthenticationFailureHandler authenticationFailureHandler,
+            JwtAuthenticationTokenCookieResolver authenticationTokenCookieResolver) {
 
-        super(AUTHORIZATION_BEARER_REQUEST_HEADER_MATCHER);
+        super(requiresAuthenticationRequestMatcher);
 
         setAuthenticationManager(authenticationManager);
         setAuthenticationSuccessHandler(NOOP_AUTH_SUCCESS_HANDLER);
         setAuthenticationFailureHandler(authenticationFailureHandler);
+
+        this.authenticationTokenCookieResolver = authenticationTokenCookieResolver;
     }
 
     @Override
@@ -49,59 +57,41 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
             HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
 
-        String bearerToken = (String) request.getAttribute(BEARER_TOKEN_ATTRIBUTE);
-        Assert.hasText(bearerToken, "bearerToken should have contained some text.");
+        Optional<String> bearerToken = Optional.empty();
 
-        Authentication authRequest = JwtAuthenticationToken.of(bearerToken);
-
-        Authentication authResult;
-        try {
-            authResult = getAuthenticationManager().authenticate(authRequest);
-        } finally {
-            request.removeAttribute(BEARER_TOKEN_ATTRIBUTE);
+        if (AJAX_REQUEST_MATCHER.matches(request)) {
+            bearerToken = authenticationTokenCookieResolver.resolveToken(request);
+        } else if (AUTHORIZATION_HEADER_MATCHER.matches(request)) {
+            bearerToken = extractFromAuthorizationHeader(request);
         }
 
-        return authResult;
+        return bearerToken.map(JwtAuthenticationToken::of)
+                .map(getAuthenticationManager()::authenticate)
+                .orElseThrow(() -> new BadCredentialsException("Null or empty token"));
     }
 
-    public void setIgnoredRequestMatchers(List<RequestMatcher> ignoredRequests) {
-        RequestMatcher requestMatcher = new AndRequestMatcher(
-                AUTHORIZATION_BEARER_REQUEST_HEADER_MATCHER,
-                new NegatedRequestMatcher(new OrRequestMatcher(ignoredRequests))
-        );
-
-        setRequiresAuthenticationRequestMatcher(requestMatcher);
-    }
-
-    private static class AuthorizationBearerRequestHeaderMatcher implements RequestMatcher {
-
-        @Override
-        public boolean matches(HttpServletRequest request) {
-            String authHeaderValue = request.getHeader(AUTHORIZATION_HEADER);
-            if (StringUtils.isEmpty(authHeaderValue)) {
-                log.debug("Authorization header is empty.");
-                return false;
-            }
-
-            if (!StringUtils.substringMatch(authHeaderValue, 0, BEARER_TOKEN_PREFIX)) {
-                log.debug(
-                        "Token prefix {} in Authorization header was not found.",
-                        BEARER_TOKEN_PREFIX
-                );
-
-                return false;
-            }
-
-            String bearerToken = authHeaderValue.substring(BEARER_TOKEN_PREFIX.length() + 1);
-            if (!StringUtils.hasText(bearerToken)) {
-                return false;
-            }
-
-            request.setAttribute(BEARER_TOKEN_ATTRIBUTE, bearerToken);
-
-            return true;
+    private Optional<String> extractFromAuthorizationHeader(HttpServletRequest request) {
+        String authHeaderValue = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.isEmpty(authHeaderValue)) {
+            log.debug("Authorization header is empty.");
+            return Optional.empty();
         }
 
+        if (!StringUtils.substringMatch(authHeaderValue, 0, BEARER_TOKEN_PREFIX)) {
+            log.debug(
+                    "Token prefix {} in Authorization header was not found.",
+                    BEARER_TOKEN_PREFIX
+            );
+
+            return Optional.empty();
+        }
+
+        String bearerToken = authHeaderValue.substring(BEARER_TOKEN_PREFIX.length() + 1);
+        if (!StringUtils.hasText(bearerToken)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(bearerToken);
     }
 
 }

src/test/java/com/springuni/hermes/core/security/authn/jwt/JwtAuthenticationFilterTest.java
@@ -2,8 +2,9 @@ package com.springuni.hermes.core.security.authn.jwt;
 
 import static com.springuni.hermes.core.security.authn.jwt.JwtAuthenticationFilter.AUTHORIZATION_HEADER;
 import static com.springuni.hermes.core.security.authn.jwt.JwtAuthenticationFilter.BEARER_TOKEN_PREFIX;
+import static com.springuni.hermes.core.web.AjaxRequestMatcher.X_REQUESTED_WITH_HEADER;
+import static com.springuni.hermes.core.web.AjaxRequestMatcher.X_REQUESTED_WITH_VALUE;
 import static java.util.Collections.emptySet;
-import static java.util.Collections.singletonList;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import static org.junit.Assert.assertEquals;
@@ -13,6 +14,7 @@ import static org.mockito.BDDMockito.then;
 
 import com.springuni.hermes.core.security.authn.user.UserIdAuthenticationToken;
 import com.springuni.hermes.test.web.BaseFilterTest;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +28,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class JwtAuthenticationFilterTest extends BaseFilterTest {
@@ -33,10 +36,13 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
     private static final Authentication AUTHENTICATED_TOKEN =
             UserIdAuthenticationToken.of(1L, emptySet());
 
-    private static final String IGNORED_PATH = "/ignored";
+    private static final String PUBLIC_PATH = "/public";
 
     @Mock
     private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtAuthenticationTokenCookieResolver authenticationTokenCookieResolver;
 
     @Mock
     private SecurityContext securityContext;
@@ -53,17 +59,17 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
         SecurityContextHolder.setContext(securityContext);
 
         jwtTokenFilter = new JwtAuthenticationFilter(
+                new NegatedRequestMatcher(new AntPathRequestMatcher(PUBLIC_PATH)),
                 authenticationManager,
-                (request, response, authentication) -> response.sendError(SC_UNAUTHORIZED)
-        );
-
-        jwtTokenFilter.setIgnoredRequestMatchers(
-                singletonList(new AntPathRequestMatcher(IGNORED_PATH))
+                (request, response, authentication) -> response.sendError(SC_UNAUTHORIZED),
+                authenticationTokenCookieResolver
         );
     }
 
+    // JWT Token is given in the Authorization header
+
     @Test
-    public void givenValidJwtToken_whenDoFilter_thenOkAndContextSet() throws Exception {
+    public void givenValidJwtTokenInHeader_whenDoFilter_thenOkAndContextSet() throws Exception {
         request.addHeader(AUTHORIZATION_HEADER, BEARER_TOKEN_PREFIX + " valid");
         given(authenticationManager.authenticate(any(JwtAuthenticationToken.class)))
                 .willReturn(AUTHENTICATED_TOKEN);
@@ -77,7 +83,7 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
     }
 
     @Test
-    public void givenValidJwtTokenAndIgnoredPath_whenDoFilter_thenOkAndContextSet()
+    public void givenValidJwtTokenInHeaderAndIgnoredPath_whenDoFilter_thenOkAndContextSet()
             throws Exception {
 
         request.addHeader(AUTHORIZATION_HEADER, BEARER_TOKEN_PREFIX + " valid");
@@ -87,7 +93,7 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
         given(authenticationManager.authenticate(any(JwtAuthenticationToken.class)))
                 .willReturn(AUTHENTICATED_TOKEN);
 
-        request.setPathInfo(IGNORED_PATH);
+        request.setPathInfo(PUBLIC_PATH);
         jwtTokenFilter.doFilter(request, response, filterChain);
 
         assertEquals(SC_OK, response.getStatus());
@@ -97,7 +103,7 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
     }
 
     @Test
-    public void givenInvalidJwtToken_whenDoFilter_thenUnauthorizedAndNoContextSet()
+    public void givenInvalidJwtTokenInHeader_whenDoFilter_thenUnauthorizedAndNoContextSet()
             throws Exception {
 
         request.addHeader(AUTHORIZATION_HEADER, BEARER_TOKEN_PREFIX + " invalid");
@@ -112,11 +118,74 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
         then(securityContext).shouldHaveZeroInteractions();
     }
 
+    // JWT Token is given in two cookies + X-Requested-With is set
+
     @Test
-    public void givenNoJwtToken_whenDoFilter_thenOkAndNoContextSet() throws Exception {
+    public void givenValidJwtTokenInCookie_whenDoFilter_thenOkAndContextSet() throws Exception {
+        request.addHeader(X_REQUESTED_WITH_HEADER, X_REQUESTED_WITH_VALUE);
+
+        given(authenticationTokenCookieResolver.resolveToken(request))
+                .willReturn(Optional.of("valid"));
+
+        given(authenticationManager.authenticate(any(JwtAuthenticationToken.class)))
+                .willReturn(AUTHENTICATED_TOKEN);
+
         jwtTokenFilter.doFilter(request, response, filterChain);
 
         assertEquals(SC_OK, response.getStatus());
+        assertJwtAuthenticationToken("valid");
+
+        then(securityContext).should().setAuthentication(AUTHENTICATED_TOKEN);
+    }
+
+    @Test
+    public void givenValidJwtTokenInCookieAndIgnoredPath_whenDoFilter_thenOkAndContextSet()
+            throws Exception {
+
+        request.addHeader(X_REQUESTED_WITH_HEADER, X_REQUESTED_WITH_VALUE);
+
+        given(authenticationTokenCookieResolver.resolveToken(request))
+                .willReturn(Optional.of("valid"));
+
+        // TODO: In newer Mockito versions, there's a better way to enable lenient stubbing for a specific test method
+        // Ref: http://blog.mockito.org/2018/07/new-mockito-api-lenient.html
+        given(authenticationManager.authenticate(any(JwtAuthenticationToken.class)))
+                .willReturn(AUTHENTICATED_TOKEN);
+
+        request.setPathInfo(PUBLIC_PATH);
+        jwtTokenFilter.doFilter(request, response, filterChain);
+
+        assertEquals(SC_OK, response.getStatus());
+
+        then(authenticationManager).shouldHaveZeroInteractions();
+        then(securityContext).shouldHaveZeroInteractions();
+    }
+
+    @Test
+    public void givenInvalidJwtTokenInCookie_whenDoFilter_thenUnauthorizedAndNoContextSet()
+            throws Exception {
+
+        request.addHeader(X_REQUESTED_WITH_HEADER, X_REQUESTED_WITH_VALUE);
+
+        given(authenticationTokenCookieResolver.resolveToken(request))
+                .willReturn(Optional.of("invalid"));
+
+        given(authenticationManager.authenticate(any(JwtAuthenticationToken.class)))
+                .willThrow(BadCredentialsException.class);
+
+        jwtTokenFilter.doFilter(request, response, filterChain);
+
+        assertEquals(SC_UNAUTHORIZED, response.getStatus());
+        assertJwtAuthenticationToken("invalid");
+
+        then(securityContext).shouldHaveZeroInteractions();
+    }
+
+    @Test
+    public void givenNoJwtToken_whenDoFilter_thenUnauthorizedAndNoContextSet() throws Exception {
+        jwtTokenFilter.doFilter(request, response, filterChain);
+
+        assertEquals(SC_UNAUTHORIZED, response.getStatus());
 
         then(authenticationManager).shouldHaveZeroInteractions();
         then(securityContext).shouldHaveZeroInteractions();

src/main/java/com/springuni/hermes/core/security/authn/jwt/JwtAuthenticationServiceImpl.java
@@ -25,6 +25,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import lombok.NonNull;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.Authentication;
@@ -32,6 +33,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.www.NonceExpiredException;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by lcsontos on 5/17/17.
@@ -54,7 +56,7 @@ public class JwtAuthenticationServiceImpl implements JwtAuthenticationService {
     }
 
     @Override
-    public String createJwtToken(Authentication authentication, int minutes) {
+    public String createJwtToken(@NonNull Authentication authentication, int minutes) {
         String authorities = authentication.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
@@ -83,6 +85,10 @@ public class JwtAuthenticationServiceImpl implements JwtAuthenticationService {
 
     @Override
     public Authentication parseJwtToken(String jwtToken) throws AuthenticationException {
+        if (!StringUtils.hasText(jwtToken)) {
+            throw new BadCredentialsException("Null or empty token.");
+        }
+
         JWTClaimsSet claimsSet;
         try {
             SignedJWT signedJWT = SignedJWT.parse(jwtToken);
@@ -123,7 +129,6 @@ public class JwtAuthenticationServiceImpl implements JwtAuthenticationService {
                         .map(it -> it.map(SimpleGrantedAuthority::new))
                         .map(it -> it.collect(toSet()))
                         .orElse(emptySet());
-
 
         return UserIdAuthenticationToken.of(userId, authorities);
     }

src/main/java/com/springuni/hermes/core/web/AjaxRequestMatcher.java
@@ -1,0 +1,21 @@
+package com.springuni.hermes.core.web;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.NonNull;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class AjaxRequestMatcher implements RequestMatcher {
+
+    public static final String X_REQUESTED_WITH_HEADER = "X-Requested-With";
+    public static final String X_REQUESTED_WITH_VALUE = "XMLHttpRequest";
+
+    private final RequestMatcher delegate =
+            new RequestHeaderRequestMatcher(X_REQUESTED_WITH_HEADER, X_REQUESTED_WITH_VALUE);
+
+    @Override
+    public boolean matches(@NonNull HttpServletRequest request) {
+        return delegate.matches(request);
+    }
+
+}

src/test/java/com/springuni/hermes/core/security/authn/jwt/JwtAuthenticationServiceTest.java
@@ -34,6 +34,12 @@ public class JwtAuthenticationServiceTest {
     @Autowired
     private JwtAuthenticationService jwtAuthenticationService;
 
+    @Test(expected = IllegalArgumentException.class)
+    public void givenNullPrincipal_whenCreateJwtToken_thenBadCredentialsException() {
+        String jwtToken = jwtAuthenticationService.createJwtToken(null, 1);
+        jwtAuthenticationService.parseJwtToken(jwtToken);
+    }
+
     @Test(expected = BadCredentialsException.class)
     public void givenInvalidPrincipal_whenCreateJwtToken_thenBadCredentialsException() {
         Authentication authentication =
@@ -75,6 +81,11 @@ public class JwtAuthenticationServiceTest {
     @Test(expected = BadCredentialsException.class)
     public void givenInvalidPrincipal_whenParseJwtToken_thenBadCredentialsException() {
         jwtAuthenticationService.parseJwtToken(JWT_TOKEN_INVALID);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void givenNullPrincipal_whenParseJwtToken_thenBadCredentialsException() {
+        jwtAuthenticationService.parseJwtToken(null);
     }
 
     @Test(expected = NonceExpiredException.class)

src/test/java/com/springuni/hermes/core/web/AjaxRequestMatcherTest.java
@@ -1,0 +1,38 @@
+package com.springuni.hermes.core.web;
+
+import static com.springuni.hermes.core.web.AjaxRequestMatcher.X_REQUESTED_WITH_HEADER;
+import static com.springuni.hermes.core.web.AjaxRequestMatcher.X_REQUESTED_WITH_VALUE;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.springuni.hermes.test.web.BaseServletTest;
+import org.junit.Test;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class AjaxRequestMatcherTest extends BaseServletTest {
+
+    private final RequestMatcher requestMatcher = new AjaxRequestMatcher();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenNullHttpRequest_whenMatches_thenIllegalArgumentException() {
+        requestMatcher.matches(null);
+    }
+
+    @Test
+    public void givenNoHeader_whenMatches_thenFalse() {
+        assertFalse(requestMatcher.matches(request));
+    }
+
+    @Test
+    public void givenHeaderWithInvalidValue_whenMatches_thenFalse() {
+        request.addHeader(X_REQUESTED_WITH_HEADER, "foo");
+        assertFalse(requestMatcher.matches(request));
+    }
+
+    @Test
+    public void givenHeaderWithValidValue_whenMatches_thenTrue() {
+        request.addHeader(X_REQUESTED_WITH_HEADER, X_REQUESTED_WITH_VALUE);
+        assertTrue(requestMatcher.matches(request));
+    }
+
+}

commit bf301e2e63089b85db29e2ead671f9af78835275

Fix regression caused by 8570db62 (#45)

src/main/java/com/springuni/hermes/core/security/authn/WebSecurityConfig.java
@@ -47,7 +47,7 @@ import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 

src/main/java/com/springuni/hermes/core/security/authn/jwt/JwtAuthenticationFilter.java
@@ -6,13 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.StringUtils;
 
@@ -26,10 +24,12 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
 
     public static final String BEARER_TOKEN_PREFIX = "Bearer";
 
+    private static final String BEARER_TOKEN_ATTRIBUTE = "bearer_token";
+
     private static final RequestMatcher AJAX_REQUEST_MATCHER = new AjaxRequestMatcher();
 
-    private static final RequestMatcher AUTHORIZATION_HEADER_MATCHER =
-            new RequestHeaderRequestMatcher(AUTHORIZATION_HEADER);
+    private static final RequestMatcher AUTHORIZATION_BEARER_REQUEST_HEADER_MATCHER =
+            new AuthorizationBearerRequestHeaderMatcher();
 
     private static final AuthenticationSuccessHandler NOOP_AUTH_SUCCESS_HANDLER =
             (request, response, authentication) -> {
@@ -57,17 +57,31 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
             HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
 
-        Optional<String> bearerToken = Optional.empty();
+        Optional<String> bearerToken =
+                Optional.ofNullable((String) request.getAttribute(BEARER_TOKEN_ATTRIBUTE));
 
-        if (AJAX_REQUEST_MATCHER.matches(request)) {
+        if (!bearerToken.isPresent() && AJAX_REQUEST_MATCHER.matches(request)) {
             bearerToken = authenticationTokenCookieResolver.resolveToken(request);
-        } else if (AUTHORIZATION_HEADER_MATCHER.matches(request)) {
-            bearerToken = extractFromAuthorizationHeader(request);
         }
 
         return bearerToken.map(JwtAuthenticationToken::of)
                 .map(getAuthenticationManager()::authenticate)
-                .orElseThrow(() -> new BadCredentialsException("Null or empty token"));
+                .orElse(null);
+    }
+
+    @Override
+    protected boolean requiresAuthentication(
+            HttpServletRequest request, HttpServletResponse response) {
+
+        if (!super.requiresAuthentication(request, response)) {
+            return false;
+        }
+
+        if (AJAX_REQUEST_MATCHER.matches(request)) {
+            return true;
+        }
+
+        return AUTHORIZATION_BEARER_REQUEST_HEADER_MATCHER.matches(request);
     }
 
     private Optional<String> extractFromAuthorizationHeader(HttpServletRequest request) {
@@ -92,6 +106,37 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
         }
 
         return Optional.of(bearerToken);
+    }
+
+    private static class AuthorizationBearerRequestHeaderMatcher implements RequestMatcher {
+
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            String authHeaderValue = request.getHeader(AUTHORIZATION_HEADER);
+            if (StringUtils.isEmpty(authHeaderValue)) {
+                log.debug("Authorization header is empty.");
+                return false;
+            }
+
+            if (!StringUtils.substringMatch(authHeaderValue, 0, BEARER_TOKEN_PREFIX)) {
+                log.debug(
+                        "Token prefix {} in Authorization header was not found.",
+                        BEARER_TOKEN_PREFIX
+                );
+
+                return false;
+            }
+
+            String bearerToken = authHeaderValue.substring(BEARER_TOKEN_PREFIX.length() + 1);
+            if (!StringUtils.hasText(bearerToken)) {
+                return false;
+            }
+
+            request.setAttribute(BEARER_TOKEN_ATTRIBUTE, bearerToken);
+
+            return true;
+        }
+
     }
 
 }

src/test/java/com/springuni/hermes/core/security/authn/jwt/JwtAuthenticationFilterTest.java
@@ -182,10 +182,17 @@ public class JwtAuthenticationFilterTest extends BaseFilterTest {
     }
 
     @Test
-    public void givenNoJwtToken_whenDoFilter_thenUnauthorizedAndNoContextSet() throws Exception {
+    public void givenNoJwtToken_whenDoFilter_thenFilterProceeds() throws Exception {
         jwtTokenFilter.doFilter(request, response, filterChain);
 
-        assertEquals(SC_UNAUTHORIZED, response.getStatus());
+        then(authenticationManager).shouldHaveZeroInteractions();
+        then(securityContext).shouldHaveZeroInteractions();
+    }
+
+    @Test
+    public void givenNoJwtTokenAndIgnoredPath_whenDoFilter_thenFilterProceeds() throws Exception {
+        request.setPathInfo(PUBLIC_PATH);
+        jwtTokenFilter.doFilter(request, response, filterChain);
 
         then(authenticationManager).shouldHaveZeroInteractions();
         then(securityContext).shouldHaveZeroInteractions();