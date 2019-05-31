250b8c9994489ee973cfb173a26efa4443f8cb5d
Laszlo Csontos <github@laszlocsontos.com>
2019-05-31 13:17:51 +0200

    Improve io.relinkr.core.scheduling (#51)

--- relinkr-api/src/main/java/io/relinkr/core/scheduling/AsyncConfig.java
+++ relinkr-api/src/main/java/io/relinkr/core/scheduling/AsyncConfig.java
@@ -26,13 +26,18 @@ import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+/**
+ * Spring configuration to enable async execution, that is methods annotated with
+ * {@link org.springframework.scheduling.annotation.Async} will be executed by using the thread
+ * pool defined here.
+ */
 @EnableAsync
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
 
   public static final String THREAD_NAME_PREFIX = "async-exec-";
 
-  // TODO: Make execturor properties configurable
+  // TODO: Make executor properties configurable
   private static final int CORE_POOL_SIZE = 5;
   private static final int MAX_POOL_SIZE = 50;
   private static final int QUEUE_CAPACITY = 1000;
