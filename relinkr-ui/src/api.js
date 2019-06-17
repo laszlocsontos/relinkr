laszlocsontos/relinkr

commit f3c7b6575f91cff134caad55de619c824e5eecfd

Cleanup - auth token isn't in use any more (#51)

relinkr-ui/src/api.js
@@ -47,20 +47,20 @@ const fetch = (method, endpoint, authToken, headers = {}, params = {},
   return axios(config);
 };
 
-export const get = ({endpoint, authToken, params, headers}) =>
-    fetch('GET', endpoint, authToken, headers, params);
+export const get = ({endpoint, params, headers}) =>
+    fetch('GET', endpoint, headers, params);
 
-export const head = ({endpoint, authToken, params, headers}) =>
-    fetch('HEAD', endpoint, authToken, headers, params);
+export const head = ({endpoint, params, headers}) =>
+    fetch('HEAD', endpoint, headers, params);
 
-export const del = ({endpoint, authToken, params, headers}) =>
-    fetch('DELETE', endpoint, authToken, headers, params);
+export const del = ({endpoint, params, headers}) =>
+    fetch('DELETE', endpoint, headers, params);
 
-export const post = ({endpoint, authToken, data, params, headers}) =>
-    fetch('POST', endpoint, authToken, headers, params, data);
+export const post = ({endpoint, data, params, headers}) =>
+    fetch('POST', endpoint, headers, params, data);
 
-export const patch = ({endpoint, authToken, data = {}, params, headers}) =>
-    fetch('PATCH', endpoint, authToken, headers, params, data);
+export const patch = ({endpoint, data = {}, params, headers}) =>
+    fetch('PATCH', endpoint, headers, params, data);
 
-export const put = ({endpoint, authToken, data, params, headers}) =>
-    fetch('PUT', endpoint, authToken, headers, params, data);
+export const put = ({endpoint, data, params, headers}) =>
+    fetch('PUT', endpoint, headers, params, data);

commit 045909d110e007092fd6df7f24313da05d64a101

Fix regression caused by commit f3c7b65 (#57)

relinkr-ui/src/api.js
@@ -25,7 +25,7 @@ const DEFAULT_HEADERS = {
   'X-Requested-With': 'XMLHttpRequest'
 };
 
-const fetch = (method, endpoint, authToken, headers = {}, params = {},
+const fetch = (method, endpoint, headers = {}, params = {},
     data = null) => {
   const config = {
     method: method,
@@ -34,11 +34,6 @@ const fetch = (method, endpoint, authToken, headers = {}, params = {},
     headers: _.defaults(DEFAULT_HEADERS, headers || {}),
     withCredentials: true
   };
-
-  if (!_.isEmpty(authToken)) {
-    config.headers = _.defaults(config.headers,
-        {'Authorization': `Bearer ${authToken}`});
-  }
 
   if (data) {
     config.data = data;