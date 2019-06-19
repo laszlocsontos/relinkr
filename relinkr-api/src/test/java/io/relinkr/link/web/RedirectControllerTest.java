d15ffdac1071ee034f8e729a1a2ae497148c1bd2
Laszlo Csontos <github@laszlocsontos.com>
2019-06-19 14:11:33 +0200

    Add support for Google Cloud Runtime Configuration API (#59)

--- relinkr-api/src/test/java/io/relinkr/link/web/RedirectControllerTest.java
+++ relinkr-api/src/test/java/io/relinkr/link/web/RedirectControllerTest.java
@@ -43,6 +43,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -53,6 +54,7 @@ import org.springframework.test.web.servlet.MockMvc;
     REDIRECT_NOT_FOUND_URL_PROPERTY + "=" + NOT_FOUND_URL
 })
 @WebMvcTest(controllers = RedirectController.class, secure = false)
+@ActiveProfiles("test")
 public class RedirectControllerTest {
 
   @Autowired
